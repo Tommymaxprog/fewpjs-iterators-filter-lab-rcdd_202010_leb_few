bf1926de1846b7ffcd6e0614955e5f78fc6da083
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-29 06:49:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter( possibleMatch =>
+    possibleMatch.toLowerCase() === name.toLowerCase()
+  )
+}
+
+function fuzzyMatch(drivers, testString) {
+  return drivers.filter( possibleMatch =>
+    possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+  )
+}
+
+function matchName(drivers, testName) {
+  return drivers.filter( record => record.name === testName)
+}
